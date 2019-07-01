e90111d07ffecb1b603284ff0bacd700e146412f
edz <458548791@qqj.com>
2019-07-01 09:39:45 +0800

    can pass one case

--- test/case/BaseCase.py
+++ test/case/BaseCase.py
@@ -27,43 +27,28 @@ class BaseCase(unittest.TestCase):
         url = case_data.get('url')
         data = case_data.get('data')
         expect_res = case_data.get('expect_res')
-       # method = case_data.get('method')
-      #  data_type = case_data.get('data_type')
+        method = case_data.get('method')
+       # data_type = case_data.get('data_type')
 
         headers = {
-            'Cookie': "_xsrf=2|38f0a2e1|dfc607a554df1c8a6b3d9a0a20e55d56|1560909123; boss_storagelist_show_type=1; boss_data_show_type=1; boss_fontsize_show_type=2; Hm_lvt_0f18425e4dfbeacd7a507a2d6e82e76c=1561337982,1561424888,1561597329,1561684209; passport=2|1:0|10:1561687083|8:passport|24:NDUzMDIwNHwxNTYxNjg3MDgz|c38a5ed352ae18b678b9385689aef2a604a7f687ea780865d8b98bb57cd6a99d; passport_hash=" + "2|1:0|10:1561687083|13:passport_hash|44:ZGM3MGFlNWRlZWI1MDUzMGY0OGE0MDRkNzgzYWJkODI=|9b59b7e518fc3dfa474aee518538bdba74cdbe2ee244b2b4c9454b1b48ce40a2" + "; pfshop_id=2|1:0|10:1561687096|9:pfshop_id|4:MTA1|f98aa9a4b2d0240d2cea075497585258a42e0f3fe3a082aeff3c926669e20d38; Hm_lpvt_0f18425e4dfbeacd7a507a2d6e82e76c=1561687097",
+            'Cookie':"_xsrf=2|38f0a2e1|dfc607a554df1c8a6b3d9a0a20e55d56|1560909123; passport_hash="+"2|1:0|10:1561797546|13:passport_hash|44:ZGM3MGFlNWRlZWI1MDUzMGY0OGE0MDRkNzgzYWJkODI=|0a7b5201fa650cac1225f4b0fd6d244e326ca4d9fd5d14bc900bb92790926b76"+"; passport=2|1:0|10:1561797546|8:passport|24:NDUzMDIwNHwxNTYxNzk3NTQ2|9cae89e4544551eec6d7b461331b4da38c1b9bb124d75bba18725f003c3b446a; boss_fontsize_show_type=2; boss_data_show_type=1; boss_storagelist_show_type=1; Hm_lvt_0f18425e4dfbeacd7a507a2d6e82e76c=1561597329,1561684209,1561789248,1561944317; pfshop_id=2|1:0|10:1561944718|9:pfshop_id|4:NzI4|4027a599f4dc5a35cfe0a85ec9290eda7b9d9e47a56d685ed10efe6c631ef685; Hm_lpvt_0f18425e4dfbeacd7a507a2d6e82e76c=1561944726",
             'user-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
         }
 
-        """
+        expect_res = json.loads(expect_res)
+        data = json.loads(data)
         if method.upper() == "GET":
             res = requests.get(url=url, data=data, headers=headers)
         elif method.upper() == "POST":
             res = requests.post(url=url, data=data, headers=headers)
-
-        expect_res = json.loads(expect_res)
         reponse_text = json.loads(res.text)
-        if operator.eq(reponse_text,expect_res):
+        if operator.eq(reponse_text, expect_res):
             result = "测试通过"
-        else :
+        else:
             result = "测试失败"
-            """
-        result = "测试失败"
 
-        data = json.loads(data)
-        res = requests.post(url=url, data=data, headers=headers)
-        logging.info(expect_res)
-        expect_res = json.loads(expect_res)
-        logging.info(res)
-        reponse_text = json.loads(res.text)
-
-        log_case_info('test_accountant_cost_avg_price', url, data, expect_res, reponse_text,result)
+        log_case_info('test_accountant_cost_avg_price', url, data, expect_res, reponse_text, result)
         self.assertEqual(reponse_text, expect_res)  # 断言
-
-
-
-
-
 
 if __name__ == "__main__":
     unittest.main()
